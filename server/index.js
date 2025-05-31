f912fc7b7ffbd4272b7a8597a5b257973cb76bcb
shobhit7098 <sharma.shobhit7060@gmail.com>
2025-05-31 20:50:39 +0530

    ResolveError1

--- server/index.js
+++ server/index.js
@@ -1,31 +1,8 @@
-// require('dotenv').config();
-// const connectDB = require('./config/db');
-// const express = require('express');
-// const cors = require('cors');
-// const reviewRoutes = require('./routes/reviewRoutes'); // Fixed path
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/reviews', reviewRoutes);
-
-// // Connect to DB and start server
-// connectDB().then(() => {
-//   const PORT = process.env.PORT || 5000;
-//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// });
-
-
 require('dotenv').config();
-const path = require('path');
+const connectDB = require('./config/db');
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const reviewRoutes = require('./routes/reviewRoutes');
+const reviewRoutes = require('./routes/reviewRoutes'); // Fixed path
 
 const app = express();
 
@@ -35,13 +12,6 @@ app.use(express.json());
 
 // Routes
 app.use('/api/reviews', reviewRoutes);
-
-// 🟡 Serve static React files
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
 
 // Connect to DB and start server
 connectDB().then(() => {
