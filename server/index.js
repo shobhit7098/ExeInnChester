bb247dd16c542635b614799c0c7077d9e14a2f55
shobhit7098 <sharma.shobhit7060@gmail.com>
2025-05-31 20:10:34 +0530

    gallery2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,25 @@
+// require('dotenv').config();
+// const connectDB = require('./config/db');
+// const express = require('express');
+// const cors = require('cors');
+// const reviewRoutes = require('./routes/reviewRoutes'); // Fixed path
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/reviews', reviewRoutes);
+
+// // Connect to DB and start server
+// connectDB().then(() => {
+//   const PORT = process.env.PORT || 5000;
+//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// });
+
+
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
